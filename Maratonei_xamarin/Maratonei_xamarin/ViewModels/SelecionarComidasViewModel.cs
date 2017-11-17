dbdeb14644964e7fd9647c5f28367f8cd0aaa897
Vinicius <viniciuslln@outlook.com>
2017-11-17 11:34:10 +0000

    Update SelecionarComidasViewModel.cs

--- Maratonei_xamarin/Maratonei_xamarin/ViewModels/SelecionarComidasViewModel.cs
+++ Maratonei_xamarin/Maratonei_xamarin/ViewModels/SelecionarComidasViewModel.cs
@@ -81,16 +81,22 @@ namespace Maratonei_xamarin.ViewModels
             GLPKInput inp = new GLPKInput();
 
             //Objetivo
+            // FO -> max(z) = x1 + x2 + x3 + x4 + …. + xn
+            // Todos terão valor 1
             inp.Objective = new GLPKObjective(Enumerable.Repeat(1.0, Comidas.Count).ToList()) { Operation = GLPKObjective.Operator.Maxmize };
-
             //variaveis
             inp.Variables = Comidas.Select(a => a.Nome).ToList();
 
+            // R1 = a1.x1 + b1.x2 + c1.x3 + … + n1.xn <= numEpiSerie1
+            //R2 = a2.x1 + b2.x2 + c2.x3 + … + n2.xn <= numEpiSerie2
+            //...
+            //RM = am.x1 + bm.x2 + cm.x3 + … + nm.xn <= numEpiSerieM
+            // G_SolucaoList == séries maratonadas
             foreach(var item in G_SolucaoList)
             {
                 var lista = new List<double>();
                 lista.AddRange(Comidas.Select((t, j) => t.ComidaSeries.First(a => a.Serie.Equals(item.Show.Title)).Quantidade) );
-
+                // Disponibilidade será número de episódios por série
                 inp.Restrictions.Add(new GLPKRestriction
                 {
                     Disponibility = item.Solucao,
@@ -99,11 +105,14 @@ namespace Maratonei_xamarin.ViewModels
                 });
             }
 
+            // R0 = 0 <= x1;x2;x3 … xn <= maxDeLanche1 … maxDeLancheM
             for (int i = 0; i < Comidas.Count; i++)
             {
                 if (Comidas[i].MaxPorEpi <= 0) continue;
                 var lista = new List<double>();
+                // Variável atual terá valor 1 e demais 0 
                 lista.AddRange(Comidas.Select((t, j) => i == j ? 1.0 : 0.0));
+                // Disponibilidade será a quantidade máxima consumida por episódio
                 inp.Restrictions.Add(new GLPKRestriction
                 {
                     Disponibility = Comidas[i].MaxPorEpi,
@@ -111,53 +120,6 @@ namespace Maratonei_xamarin.ViewModels
                     Operation = GLPKRestriction.Operator.LessOrEqual
                 });
             }
-
-            ////R1 = lancheX + lancheY + lancheW + lancheZ <= quantLanchesTotal
-            //var quantTotal = 0.0;
-            //foreach (var item in Comidas)
-            //{
-            //    quantTotal += item.Quantidade;
-            //}
-            //inp.Restrictions.Add(new GLPKRestriction {
-            //    Disponibility = quantTotal,
-            //    Values = Comidas.Select(a => a.Quantidade).ToList(),
-            //    Operation = GLPKRestriction.Operator.LessOrEqual
-            //});
-
-            ////R2 = lancheX + lancheY + lancheW + lancheZ >= minDeLanchePorEpisodio
-            //for (int i = 0; i < Comidas.Count; i++)
-            //{
-            //    var lista = new List<double>();
-            //    lista.AddRange(Comidas.Select((t, j) => i == j ? 1.0 : 0.0));
-            //    inp.Restrictions.Add(new GLPKRestriction
-            //    {
-            //        Disponibility = Comidas[i].Min,
-            //        Values = lista,
-            //        Operation = GLPKRestriction.Operator.GreaterOrEqual
-            //    });
-            //}
-
-            ////R3 = lancheX + lancheY + lancheW + lancheZ >= numEpis maratonados
-            //inp.Restrictions.Add(new GLPKRestriction
-            //{
-            //    Disponibility = Maratona,
-            //    Values = Comidas.Select(a => a.Quantidade).ToList(),
-            //    Operation = GLPKRestriction.Operator.GreaterOrEqual
-            //});
-
-            //R4 = x; y; w; z => 0
-            //for (int i = 0; i < Comidas.Count; i++)
-            //{
-            //    var lista = new List<double>();
-            //    lista.AddRange(Comidas.Select((t, j) => i == j ? 1.0 : 0.0 ));
-            //    inp.Restrictions.Add(new GLPKRestriction
-            //    {
-            //        Disponibility = 0,
-            //        Values = lista,
-            //        Operation = GLPKRestriction.Operator.GreaterOrEqual
-            //    });
-            //}
-
 
             return inp;
         }
